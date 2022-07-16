6ead1e623c13db8590db76888f5c65881d9289c2
Yash <yash_pogosyan1999@mail.ru>
2022-07-16 20:55:09 +0400

    lesson one

---
+++ src/Hello.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class Hello extends Component{
+    constructor(props){
+        super()
+    }
+    render(){
+        return(
+            <div>
+            <h2>Hello</h2>
+            <h2>world</h2>
+            </div>    
+        )
+    }
+}
+
+export default Hello;
